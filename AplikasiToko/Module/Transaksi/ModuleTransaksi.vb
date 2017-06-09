db19eabd5206913d4bc8afd8aee81c9732f15980
Ceater <johan079a19@yahoo.co.id>
2017-06-09 17:50:30 +0700

    Mulai Modul Transaksi + Query

---
+++ AplikasiToko/Module/Transaksi/ModuleTransaksi.vb
@@ -0,0 +1,16 @@
+﻿Imports System.Data.Sql
+Imports System.Data.SqlClient
+
+Module ModuleTransaksi
+    Sub insertHJual()
+
+    End Sub
+
+    Sub insertDJual()
+
+    End Sub
+
+    Sub insertPembayaran()
+
+    End Sub
+End Module
